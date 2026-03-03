1c78642c42c0230ca6f3b9376f1733226a9387ce
Victor Malmberg Schimmell <fmt568@alumni.ku.dk>
2026-03-03 18:05:24 +0100

    Initial commit

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    // public DbSet<Product> Products => Set<Product>();
+}
