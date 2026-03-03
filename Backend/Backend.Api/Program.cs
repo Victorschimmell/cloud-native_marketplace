f1eae9f9397ee265c92078284e2894c3c6bbb3b0
Victor Malmberg Schimmell <fmt568@alumni.ku.dk>
2026-03-03 19:16:45 +0100

    Added Scalar

--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -1,5 +1,6 @@
 using Backend.Api.Extensions;
 using Backend.Infrastructure;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("/scalar");
 }
 
 if (app.Configuration.GetValue<bool>("Database:ApplyMigrationsOnStartup"))
