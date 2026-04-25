ede2f03995f3a2b4b87145712780042139dac3cb
Victor Malmberg Schimmell <fmt568@alumni.ku.dk>
2026-04-25 12:19:19 +0200

    Fixed some skeleton implementations of interfaces

--- Backend/tests/Backend.UnitTests/Application/Services/ServiceConstructorTests.cs
+++ Backend/tests/Backend.UnitTests/Application/Services/ServiceConstructorTests.cs
@@ -8,7 +8,7 @@ public sealed class ServiceConstructorTests
     [Fact]
     public void ProductService_Throws_When_ProductRepository_Is_Null()
     {
-        Assert.Throws<ArgumentNullException>(() => new ProductService(null!));
+        Assert.Throws<ArgumentNullException>(() => new ProductService(null!, new FakeProductListingRepository()));
     }
 
     [Fact]
@@ -28,7 +28,7 @@ public sealed class ServiceConstructorTests
     {
         _ = new CustomerService(new FakeCustomerRepository());
         _ = new SellerService(new FakeSellerRepository());
-        _ = new ProductService(new FakeProductRepository());
+        _ = new ProductService(new FakeProductRepository(), new FakeProductListingRepository());
         _ = new CategoryService(new FakeProductCategoryRepository());
         _ = new OrderService(new FakeOrderRepository(), new FakeOrderItemRepository());
         _ = new PaymentService(new FakePaymentRepository(), new FakeOrderRepository(), new FakeDateTimeProvider());
