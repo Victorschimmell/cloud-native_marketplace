97c3d94dbfd8f4d1dbf6c0dc282b5df65312efb6
iwtba4188 <X69599596@gmail.com>
2026-04-30 12:24:57 +0200

    feat: Customer mapping

--- Backend/Backend.Api/Mappings/User/Registration/RegistrationMappingExtensions.cs
+++ Backend/Backend.Api/Mappings/User/Registration/RegistrationMappingExtensions.cs
@@ -5,4 +5,18 @@ namespace Backend.Api.Mappings.User.Registration;
 
 public static class RegistrationMappingExtensions
 {
+    public static CustomerResponse ToResponse(this App.CustomerDto customer)
+    {
+        return new CustomerResponse
+        {
+            Id = customer.Id,
+            UserId = customer.UserId,
+            FirstName = customer.FirstName,
+            LastName = customer.LastName,
+            Phone = customer.Phone,
+            DefaultAddressId = customer.DefaultAddressId,
+            OlistCustomerId = customer.OlistCustomerId,
+            OlistCustomerUniqueId = customer.OlistCustomerUniqueId
+        };
+    }
 }
