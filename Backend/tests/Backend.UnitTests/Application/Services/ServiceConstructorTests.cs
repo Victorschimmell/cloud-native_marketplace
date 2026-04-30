4b6f6ceef253617b426b1b95d6fee5c5658edc85
iwtba4188 <X69599596@gmail.com>
2026-04-30 21:38:24 +0200

    test: Fixed minor issue in unit tests

--- Backend/tests/Backend.UnitTests/Application/Services/ServiceConstructorTests.cs
+++ Backend/tests/Backend.UnitTests/Application/Services/ServiceConstructorTests.cs
@@ -33,7 +33,7 @@ public sealed class ServiceConstructorTests
         _ = new OrderService(new FakeOrderRepository(), new FakeOrderItemRepository());
         _ = new PaymentService(new FakePaymentRepository(), new FakeOrderRepository(), new FakeDateTimeProvider());
         _ = new ReviewService(new FakeOrderReviewRepository(), new FakeOrderRepository(), new FakeDateTimeProvider());
-        _ = new CartService(new FakeCartRepository(), null!, new FakeDateTimeProvider(), new FakeCurrencyConversionService(), new FakeUnitOfWork());
+        _ = new CartService(new FakeCartRepository(), new FakeProductListingRepository(), new FakeDateTimeProvider(), new FakeCurrencyConversionService(), new FakeUnitOfWork());
         _ = new CheckoutService(new FakeCartRepository(), new FakeOrderRepository(), new FakePaymentRepository(), new FakeDateTimeProvider(), new FakeCurrencyConversionService());
         _ = new AnalyticsService(new FakeOrderRepository(), new FakeDateTimeProvider());
         _ = new AuthService(new FakeUserAccountRepository(), new FakePasswordHasher(), new FakeAuthTokenGenerator(), new FakeDateTimeProvider());
