c7c5a05d94b191fa0f51a6eba0bdda2945b8886f
iwtba4188 <X69599596@gmail.com>
2026-05-05 15:53:40 +0200

    feat: Scalar authentication support

---
+++ Backend/Backend.Api/OpenApi/Transformers/BearerSecuritySchemeTransformer.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace Backend.Api.OpenApi.Transformers;
+
+// reference: https://github.com/scalar/scalar/issues/4055
+internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        var schemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+
+        if (!schemes.Any(s => s.Name == "MarketplaceBearer"))
+            return;
+
+        document.Components ??= new OpenApiComponents();
+
+        if (document.Components.SecuritySchemes == null)
+            document.Components.SecuritySchemes = new Dictionary<string, IOpenApiSecurityScheme>();
+
+        var schemeId = "Bearer";
+
+        document.Components.SecuritySchemes[schemeId] = new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization"
+        };
+
+        document.Security ??= new List<OpenApiSecurityRequirement>();
+
+        document.Security.Add(new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference(schemeId)] = new List<string>()
+        });
+    }
+}
--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -50,6 +50,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi(options =>
 {
+    options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
     options.AddOperationTransformer<DefaultResponsesTransformer>();
 });
 
@@ -76,7 +77,16 @@ app.UseSerilogRequestLogging();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .AddPreferredSecuritySchemes("Bearer")
+            .AddHttpAuthentication(
+                "Bearer",
+                auth => { }
+            )
+            .EnablePersistentAuthentication();
+    });
 }
 
 app.UseHttpsRedirection();
