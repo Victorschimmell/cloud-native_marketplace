c13aafc60a0decb624846dd6b490bf65bc3023f8
Victor Malmberg Schimmell <fmt568@alumni.ku.dk>
2026-05-20 10:15:28 +0200

    Revert "Removed not needed seller-seeding"
    This reverts commit fe95c3abaef5ae501c6db728da18d6eeb14b566b.

--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -89,6 +89,7 @@ app.UseSerilogRequestLogging();
 if (app.Environment.IsDevelopment())
 {
     await app.Services.SeedAdminDataAsync(builder.Configuration);
+    await app.Services.SeedSellerDataAsync(builder.Configuration);
 
     app.MapOpenApi();
     app.MapScalarApiReference(options =>
