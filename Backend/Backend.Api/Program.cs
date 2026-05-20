fe95c3abaef5ae501c6db728da18d6eeb14b566b
Victor Malmberg Schimmell <fmt568@alumni.ku.dk>
2026-05-20 10:14:32 +0200

    Removed not needed seller-seeding

--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -89,7 +89,6 @@ app.UseSerilogRequestLogging();
 if (app.Environment.IsDevelopment())
 {
     await app.Services.SeedAdminDataAsync(builder.Configuration);
-    await app.Services.SeedSellerDataAsync(builder.Configuration);
 
     app.MapOpenApi();
     app.MapScalarApiReference(options =>
